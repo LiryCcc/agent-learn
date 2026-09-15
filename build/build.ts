245dfd19b07dd7710d538a458f6bc378b0c54ccb
Liry <learainccc@outlook.com>
2026-09-15 21:54:07 +0800

    feat: add liry-build

---
+++ build/build.ts
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+import { resolve } from 'node:path';
+import { cwd } from 'node:process';
+import { build as defaultBuild, defineConfig, mergeConfig, type UserConfig } from 'tsdown';
+
+const c = cwd();
+
+const defaultConfig = defineConfig({
+  entry: resolve(c, 'src/index.ts'),
+  outExtensions: () => ({ js: '.js', dts: '.ts' }),
+  dts: true,
+  target: false,
+  format: 'es',
+  outDir: resolve(c, 'dist'),
+  alias: {
+    dayjs: resolve(c, 'node_modules/dayjs/esm/index.js'),
+    '@bufbuild/protobuf': resolve(c, 'node_modules/@bufbuild/protobuf/dist/esm')
+  }
+});
+
+if (import.meta.main) {
+  defaultBuild(defaultConfig);
+}
+
+export const build = (...configs: UserConfig[]) => defaultBuild(mergeConfig(defaultConfig, ...configs));
