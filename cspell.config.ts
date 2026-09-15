166dad47ecb91dc68ed80a448e800653361c0b34
Liry <learainccc@outlook.com>
2026-09-15 21:48:03 +0800

    format

--- cspell.config.ts
+++ cspell.config.ts
@@ -1,18 +1,10 @@
 import { defineConfig } from 'cspell';
 
 const cspellConfig = defineConfig({
-  ignorePaths: [
-    'node_modules',
-    '**/package.json',
-    '**/pnpm-lock.yaml',
-    '**/dist',
-  ],
+  ignorePaths: ['node_modules', '**/package.json', '**/pnpm-lock.yaml', '**/dist'],
   useGitignore: true,
   version: '0.2',
-  words: [
-    'liry',
-    'dedupe'
-  ]
+  words: ['liry', 'dedupe']
 });
 
 export default cspellConfig;
